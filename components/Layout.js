8c83281c4b0afca89ef2bf326cc162bd3e86f0a2
meastt <114757104+meastt@users.noreply.github.com>
2025-06-04 13:08:32 -0600

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
-import '../globals/styles.css';
+import '../styles/globals.css';
 
 export default function Layout({ children }) {
   return (

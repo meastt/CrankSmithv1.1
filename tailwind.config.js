d7fba747bfe8fe67ecdb2cebeea24c7f891e3de9
meastt <114757104+meastt@users.noreply.github.com>
2025-06-17 18:04:44 +0000

    Update tailwind.config.js
    safelist some global color/styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,21 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
+  safelist: [
+    'text-white',
+    'text-gray-100',
+    'text-gray-200',
+    'text-gray-300',
+    'text-gray-800',
+    'text-gray-900',
+    'bg-white',
+    'bg-gray-50',
+    'bg-gray-100',
+    'bg-gray-800',
+    'bg-gray-900',
+    'text-blue-600',
+    'text-blue-500'
+  ],
   theme: {
     extend: {},
   },

f9cbf199f0496bab61c09629e8da1317e9cb2c18
meastt <114757104+meastt@users.noreply.github.com>
2025-06-17 11:27:05 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
+  // Remove the swcMinify line if it's there
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Exclude service worker from server-side bundling
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        'sw.js': false,
+      };
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;

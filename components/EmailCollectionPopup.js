4a1a8a61f02f8d78999c3e8ca39ee7fa30641bd9
Meastt <youremail@example.com>
2025-07-22 17:48:20 -0600

    update: additional EmailCollectionPopup changes

--- components/EmailCollectionPopup.js
+++ components/EmailCollectionPopup.js
@@ -57,7 +57,7 @@ export default function EmailCollectionPopup() {
       }
 
       // Send email to backend API
-      const response = await fetch('/api/subscribe-simple', {
+      const response = await fetch('/api/early-access', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
